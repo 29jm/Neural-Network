246792cc16306980b795735688ee5cbe58a0720d
Johan Manuel <johan.manuel@live.fr>
2014-12-07 16:20:55 +0100

    replicated the original example

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@
 using namespace std;
 
 int main() {
-	NeuralNetwork nn(1, 2, 1);
+	NeuralNetwork nn(2, 2, 1);
 	vector<vector<vector<float>>> pat = {
-		{{0},{1}},
-		{{1},{0}}
+		{{0, 0}, {0}},
+		{{1, 1}, {0}},
+		{{1, 0}, {1}},
+		{{0, 1}, {1}},
 	};
 
-	nn.train(pat, 1000);
+	for (int i = 0; i < 1000; i++) {
+		nn.train(pat, 1);
+	}
+
 	nn.test(pat, true);
 }
